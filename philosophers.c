19b118c772400c9d2c435583eb4c8df0a45879f9
HHHati <basile_deleeuw@yahoo.be>
2022-09-03 18:33:47 +0200

    1.1

--- philosophers.c
+++ philosophers.c
@@ -6,19 +6,50 @@
 /*   By: Bade-lee <bade-lee@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 16:03:29 by Bade-lee          #+#    #+#             */
-/*   Updated: 2022/09/03 16:36:58 by Bade-lee         ###   ########.fr       */
+/*   Updated: 2022/09/03 17:57:31 by Bade-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+int	check_args(int argc, char **argv)
+{
+	size_t	i;
+
+	i = 1;
+	if (!(argc == 5 || argc == 6))
+		return (0);
+	while (i < argc)
+	{
+		if (ft_atoi(argv[i]) < 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	init_philo(int argc, char **argv, t_philo *philo)
+{
+	if (!check_args)
+	{
+		write(2, "philosophers: invalid arguments\n", 32);
+		exit(1);
+	}
+	philo->number = ft_atoi(argv[1]);
+	philo->time_die = ft_atoi(argv[2]);
+	philo->time_eat = ft_atoi(argv[3]);
+	philo->time_sleep = ft_atoi(argv[4]);
+	if (argc == 5)
+		philo->eat_max = 0;
+	else
+		philo->eat_max = ft_atoi(argv[5]);
+}
+
 int	main(int argc, char **argv)
 {
 	t_philo	philo;
 	
-	if (!(argc == 5 || argc == 6))
-	{
-		write(2, "philosophers: invalid arguments\n", 32);
-		return (1);
-	}
+	init_philo(argc, argv, philo);
+
+	return (0);
 }
--- utils.c
+++ utils.c
@@ -6,8 +6,74 @@
 /*   By: Bade-lee <bade-lee@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 16:03:26 by Bade-lee          #+#    #+#             */
-/*   Updated: 2022/09/03 16:03:55 by Bade-lee         ###   ########.fr       */
+/*   Updated: 2022/09/03 18:31:44 by Bade-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+static int	ft_iswhitespace(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\v'
+		|| c == '\f')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int					i;
+	int					sign;
+	unsigned long int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (ft_iswhitespace(str[i]) == 1)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+		if (result > INT_MAX / 10 && str[i] - 48
+			> (INT_MAX % 10) - (sign - 1) / 2)
+			return (-1);
+	}
+	return (result * sign);
+}
+
+int	get_start_time(void)
+{
+	struct timeval	tp;
+	int				result;
+
+	gettimeofday(&tp, NULL);
+	result = (tp.tv_sec * 1000) + (tp.tv_usec / 1000);
+	return (result);
+}
+
+int get_relative_time(t_info *info)
+{
+	int	result;
+	struct timeval	tp;
+
+	gettimeofday(&tp, NULL)
+	result = (tp.tv_sec * 1000) + (tp.tv_usec / 1000);
+	result -= info->start_time;
+	return (result);
+}
+
+void	wait_time(int time, t_info info)
+{
+	int	reach_time;
+
+	reach_time = get_relative_time(info) + time;
+	while (get_relative_time(info) < reach_time)
+		usleep(100);
+}
